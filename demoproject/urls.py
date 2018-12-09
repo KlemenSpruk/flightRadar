0ba583c6f5e7554a3dcf9102a68d7fb0a2b7407f
Klemen Špruk <spruk.klemen@gmail.com>
2018-12-09 17:37:22 +0100

    add map and load static geojson

--- demoproject/urls.py
+++ demoproject/urls.py
@@ -17,8 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
     path('', admin.site.urls),
-    # path('flightmap/', include('flightmap.urls')),
-    # path('admin/', admin.site.urls),
+    path('flightmap/', include('flightmap.urls')),
+    path('admin/', admin.site.urls),
 ]
